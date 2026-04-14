2c21daa5cf84952260822114edc0ae3ba3547ca1
elbachiryaraaly-tech <elbachiryaraaly@gmail.com>
2026-04-14 13:47:14 +0200

    Voici l'archive test java

---
+++ CalculadoraTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculadoraTest {
+
+    Calculadora calc = new Calculadora();
+
+    @Test
+    public void testSumar() {
+        assertEquals(5, calc.sumar(2, 3));
+    }
+
+    @Test
+    public void testRestar() {
+        assertEquals(1, calc.restar(3, 2));
+    }
+
+    @Test
+    public void testMultiplicar() {
+        assertEquals(6, calc.multiplicar(2, 3));
+    }
+
+    @Test
+    public void testDividir() {
+        assertEquals(2.0, calc.dividir(6, 3), 0.001);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDividirEntreCero() {
+        calc.dividir(5, 0);
+    }
+}
